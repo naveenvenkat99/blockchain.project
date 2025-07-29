58893a72666aa8f5a585a6fbeca5fbf0dfa0db04
naveenvenkat99 <naveenvenkat532@gmail.com>
2025-07-29 22:20:55 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import DashboardScreen from './screens/DashboardScreen';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Dashboard: { token: string };
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
